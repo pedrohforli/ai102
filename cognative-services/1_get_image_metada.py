9fa26b086728b7f9de4161ea3abc31f0570db897
Ignorância Zero <ignorancia.zero.2021@gmail.com>
2022-04-15 12:09:54 -0300

    image metadata

---
+++ cognative-services/1_get_image_metada.py
@@ -0,0 +1,22 @@
+import os
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
+# get the information about the connection
+subscription_key = os.environ["COMPUTER_VISION_SUBSCRIPTION_KEY"]
+endpoint = os.environ["COMPUTER_VISION_ENDPOINT"]
+
+# create a client to connect with the API
+client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+# select the image to process
+image_url = "https://pbs.twimg.com/media/FPBAfjxXMAQN2uh.jpg"
+
+# describe the image
+response = client.describe_image(image_url, max_candidates=1)
+print(response.captions[0])
+
+# get tags for the image
+tags = client.tag_image(url=image_url)
+print([tag.name for tag in tags.tags])
